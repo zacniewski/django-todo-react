1f8f738837b938be91a02a209b84061fa64a625b
Artur Zacniewski <a.zacniewski@gmail.com>
2019-07-16 21:59:50 +0200

    Serializers of  DRF added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # we whitelist localhost:3000 because that's where frontend will be served
+"""
 CORS_ORIGIN_WHITELIST = (
     'localhost:3000/'
 )
+"""
